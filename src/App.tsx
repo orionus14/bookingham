823ebd2ce7a7892328e45c182add84d2aee6aff6
orion <orion2003orion@gmail.com>
2024-12-04 12:45:44 +0200

    Added Navbar and Router paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom"
+import BookList from "./pages/BookList"
+import About from "./pages/About"
+import Contacts from "./pages/Contacts"
+import FAQ from "./pages/FAQ"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <div>
-      
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/books" element={<BookList />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/faq" element={<FAQ />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ src/components/NavItem.tsx
@@ -0,0 +1,24 @@
+import React, { act } from "react";
+import { NavLink } from "react-router-dom"
+
+interface INavItem {
+    to: string;
+    children: React.ReactNode
+}
+
+const NavItem: React.FC<INavItem> = ({ to, children }) => {
+    const activeClass = 'underline';
+
+    return (
+        <NavLink
+            to={to}
+            className={({ isActive }) =>
+               `${isActive ? activeClass : ''}` 
+            }
+        >
+            {children}
+        </NavLink>
+    )
+}
+
+export default NavItem
