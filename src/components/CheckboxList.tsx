cce7b2a663fa37bc70276f0a49dbaa0b7644c60c
orion <orion2003orion@gmail.com>
2024-12-08 17:10:26 +0200

    Added show more or less toggle

--- src/components/CheckboxList.tsx
+++ src/components/CheckboxList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface ICheckboxList {
     categories: string[];
@@ -6,20 +6,38 @@ interface ICheckboxList {
 }
 
 const CheckboxList: React.FC<ICheckboxList> = ({ categories, name }) => {
+
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    }
+
     return (
-        <ul>
-            {categories.map((category, index) => (
-                <li key={index}>
-                    <input
-                        type="checkbox"
-                        name={name}
-                        id={category}
-                        className='mr-2'
-                    />
-                    <label htmlFor={category}>{category}</label>
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul>
+                {categories.slice(0, isExpanded ? categories.length : 5)
+                    .map((category, index) => (
+                        <li key={index}>
+                            <input
+                                type="checkbox"
+                                name={name}
+                                id={category}
+                                className='mr-2'
+                            />
+                            <label htmlFor={category}>{category}</label>
+                        </li>
+                    ))}
+            </ul>
+            {categories.length > 5 && (
+                <button
+                    className='mt-2 text-blue-500 hover:underline'
+                    onClick={toggleExpand}
+                >
+                    {isExpanded ? 'Show Less' : 'Show More'}
+                </button>
+            )}
+        </>
     )
 }
 
